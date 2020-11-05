b1b7c156308958dee081e168ce570b6a1ad5c7bc
Justin Winger <justinwinger4@gmail.com>
2020-11-05 15:14:12 -0700

    setup pg

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,5 @@
 # Load the Rails application.
 require_relative 'application'
-
+# config.frameworks -= [ :active_record, :active_resource, :action_mailer]
 # Initialize the Rails application.
 Rails.application.initialize!
