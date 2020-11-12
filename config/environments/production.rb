6fe823e7259a1a2455b4cbdb59399be603da75fe
Justin Winger <justinwinger4@gmail.com>
2020-11-12 06:47:32 -0700

    production update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.config.read_encrypted_secrets = true
+  #config.config.read_encrypted_secrets = true
   # Code is not reloaded between requests.
   config.cache_classes = true
   config.serve_static_assets = true
